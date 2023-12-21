bc88e56afa187ce9096df27ee2bdc8cd6998b49e
tobiaaa <st164519@stud.uni-stuttgart.de>
2023-12-21 15:00:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,4 +10,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf;
 
 # vim config
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 ln -s ~/.dotfiles/.vimrc ~/.vimrc;
