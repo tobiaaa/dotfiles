ae6ec6ee71fbeb48bdde5f809221eb9521997022
tobiaaa <st164519@stud.uni-stuttgart.de>
2023-12-21 14:49:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,11 @@
 # zsh config
 
 # tmux config
+mkdir ~/.tmux;
+# Install tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf;
+
+# vim config
+ln -s ~/.dotfiles/.vimrc ~/.vimrc;
