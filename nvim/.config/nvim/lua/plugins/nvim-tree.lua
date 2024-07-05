7bfbd00d18b1abcbb7bb371266d03de3d481fd68
Tobias Raichle <tobias.raichle@iss.uni-stuttgart.de>
2024-07-05 16:56:05 +0200

    feat: Formatting

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,54 +1,51 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        local nvimtree = require("nvim-tree")
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    local nvimtree = require "nvim-tree"
 
-        -- recommended settings
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+    -- recommended settings
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-        nvimtree.setup({
-            view = {
-                width = 35,
-                relativenumber = true,
+    nvimtree.setup {
+      view = {
+        width = 35,
+        relativenumber = true,
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_open = "",
+              arrow_closed = "",
             },
-            renderer = {
-                indent_markers = {
-                    enable = true
-                },
-                icons = {
-                    glyphs = {
-                        folder = {
-                            arrow_open = "",
-                            arrow_closed = ""
-                        }
-                    }
-                }
-            },
-            actions = {
-                open_file = {
-                    window_picker = {
-                        enable = false
-                    },
-                },
-            },
-            filters = {
-                custom = { ".DS_Store", "__pycache__", ".git" },
-            },
-            git = {
-                ignore = false,
-            },
-        })
+          },
+        },
+      },
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      filters = {
+        custom = { ".DS_Store", "__pycache__", ".git" },
+      },
+      git = {
+        ignore = false,
+      },
+    }
 
-        local map = vim.keymap.set
+    local map = vim.keymap.set
 
-        map("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer"})
-        map("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file"})
-        map("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer"})
-        map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer"})
-
-    end
+    map("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    map("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+    map("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+  end,
 }
-
-
