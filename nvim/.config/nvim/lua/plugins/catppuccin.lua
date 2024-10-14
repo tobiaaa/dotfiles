ac01d641fb2f0f7590f8538f97b09463bf3fd11b
Tobias Raichle <toby.raichle.tr@gmail.com>
2024-10-14 16:05:47 +0200

    Added DOGE; Removed mini-map; Modified themes

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,13 +1,14 @@
 return {
     "catppuccin/nvim",
+    priority = 1000,
     config = function()
-        require("catppuccin").setup({
-            custom_highlights = function(colors)
-                return {
-                    AlphaShortcut = { fg = colors.red, bg = colors.mauve},
-                    AlphaHeader = { fg = colors.red, bg = colors.mauve},
-                }
-            end
-        })
-    end
+         require("catppuccin").setup({
+             custom_highlights = function(colors)
+                 return {
+                     AlphaShortcut = { fg = colors.red, bg = colors.mauve},
+                     AlphaHeader = { fg = colors.red, bg = colors.mauve},
+                 }
+             end
+         })
+     end
 }
