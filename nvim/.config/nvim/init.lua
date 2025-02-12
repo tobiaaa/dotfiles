3867d0858c7405c3c9a32876261bc86251dee6b0
Tobias Raichle <toby.raichle.tr@gmail.com>
2025-02-12 08:11:30 +0100

    feat: Transparancy

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,8 +6,8 @@ vim.g.maplocalleader = " "
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+    local repo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -16,20 +16,20 @@ local lazy_config = require "configs.lazy"
 
 -- load plugins
 require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
-    change_detection = {
+    {
+        "NvChad/NvChad",
+        lazy = false,
+        branch = "v2.5",
+        import = "nvchad.plugins",
+        config = function()
+            require "options"
+        end,
+        change_detection = {
             notify = false,
         }
-  },
+    },
 
-  { import = "plugins" },
+    { import = "plugins" },
 }, lazy_config)
 
 -- load theme
@@ -37,22 +37,27 @@ dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
 vim.opt.termguicolors = true
-vim.opt.colorcolumn = {80,100}
+vim.opt.colorcolumn = { 80, 100 }
 vim.opt.foldmethod = "manual"
 vim.opt.timeoutlen = 1000
+
 
 vim.env.NVIM = "1"
 
 require "nvchad.autocmds"
 
 vim.schedule(function()
-  require "mappings"
+    require "mappings"
 end)
 
--- load bbye
 local ok, _ = pcall(vim.cmd, 'colorscheme catppuccin')
 
 if not ok then
     vim.cmd 'colorscheme default'
 end
-
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none"})
