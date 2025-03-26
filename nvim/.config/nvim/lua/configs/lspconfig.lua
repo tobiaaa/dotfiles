db152f6cc6f98df6e677bfb8f6ea48db7302e774
Tobias Raichle <tobias.raichle@iss.uni-stuttgart.de>
2025-03-26 16:47:59 +0100

    feat: Added blink.cmp

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "texlab" }
+local servers = { "html", "cssls", "texlab" , "bashls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -21,6 +21,3 @@ lspconfig.pyright.setup {
   capabilities = capabilities,
   filetypes = { "python" },
 }
-
--- bash
-lspconfig.bashls.setup {}
