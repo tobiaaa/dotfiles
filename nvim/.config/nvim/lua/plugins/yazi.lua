48d1dc5450d9e65d23a479a4a26b995f519fda8f
Tobias Raichle <toby.raichle.tr@gmail.com>
2026-01-19 08:02:06 +0100

    feat: Added nvim yazi plugin

---
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,22 @@
+return {
+  "mikavilpas/yazi.nvim",
+  version = "*", -- use the latest stable version
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    {
+      "<leader>E",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>EE",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+  },
+}
