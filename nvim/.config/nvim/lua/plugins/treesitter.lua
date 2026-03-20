28511222e370c66dc23cfc3d4e2e1b06a398a166
Tobias Raichle <toby.raichle.tr@gmail.com>
2026-03-20 10:50:46 +0100

    feat: Updated branches

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    branch = "main",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
         -- New nvim-treesitter only manages parser installation.
